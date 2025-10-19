52ab5bd0312c41ac535334fe5a947d4a3948c5bc
Matthew Costa <ucosty@gmail.com>
2025-10-19 19:50:16 +0100

    Add equals ignoring case helper function for std::string strings
    The standard library functions don't include a case insensitive string
    comparison function, so this commit adds one.

---
+++ src/common/utility/string_helpers.cpp
@@ -0,0 +1,30 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright 2025 Matthew Costa
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/
+//
+//-----------------------------------------------------------------------------
+
+#include "string_helpers.h"
+
+bool is_equal_ignoring_case(const std::string &a, const std::string &b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    return std::equal(a.begin(), a.end(), b.begin(), [](const char char_a, const char char_b) {
+        return std::tolower(static_cast<unsigned char>(char_a)) == std::tolower(static_cast<unsigned char>(char_b));
+    });
+}
